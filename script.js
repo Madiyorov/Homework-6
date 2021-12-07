094227fafabce8e94f3ed47cff69bbea8e9326fc
Islom <b.madiyorov.i@mail.ru>
2021-12-07 15:39:58 +0500

    test

---
+++ script.js
@@ -0,0 +1,42 @@
+ /* ДЗ */
+
+    let name1 = prompt(' Введите имя ');
+    let age1 = +prompt(' Введите возраст ');
+    let name2 = prompt(' Введите имя ');
+    let age2 = +prompt(' Введите возраст ');
+    let name3 = prompt(' Введите имя ');
+    let age3 = +prompt(' Введите возраст ');
+    let name4 = prompt(' Введите имя ');
+    let age4 = +prompt(' Введите возраст ');
+    let name5 = prompt(' Введите имя ');
+    let age5 = +prompt(' Введите возраст ');
+
+    
+const users = {
+    'Ползователь 1': {
+        name: name1,
+        age: age1,    
+    },
+    'Ползователь 2': {
+        name: name2,
+        age: age2,    
+    },
+    'Ползователь 3': {
+        name: name3,
+        age: age3,    
+    },
+    'Ползователь 4': {
+        name: name4,
+        age: age4,
+    },
+    'Ползователь 5': {
+        name: name5,
+        age: age5,
+    }
+}
+    
+    console.log('Пользователь 1 ' + 'Имя: ' + name1 + ' Возраст: ' + age1)
+    console.log('Пользователь 2 ' + 'Имя: ' + name2 + ' Возраст: ' + age2)
+    console.log('Пользователь 3 ' + 'Имя: ' + name3 + ' Возраст: ' + age3)
+    console.log('Пользователь 4 ' + 'Имя: ' + name4 + ' Возраст: ' + age4)
+    console.log('Пользователь 5 ' + 'Имя: ' + name5 + ' Возраст: ' + age5)
