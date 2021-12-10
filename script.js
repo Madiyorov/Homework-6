0b28c9968d2026908643f18b28c926e39d60422b
Islom <b.madiyorov.i@mail.ru>
2021-12-10 09:32:11 +0500

    test3

--- script.js
+++ script.js
@@ -1,42 +1,19 @@
- /* ДЗ */
+                     /* ДЗ */
 
-    let name1 = prompt(' Введите имя ');
-    let age1 = +prompt(' Введите возраст ');
-    let name2 = prompt(' Введите имя ');
-    let age2 = +prompt(' Введите возраст ');
-    let name3 = prompt(' Введите имя ');
-    let age3 = +prompt(' Введите возраст ');
-    let name4 = prompt(' Введите имя ');
-    let age4 = +prompt(' Введите возраст ');
-    let name5 = prompt(' Введите имя ');
-    let age5 = +prompt(' Введите возраст ');
+    const obj = {}
+for(let i = 1; i <= 10; i++) {
+    let name = prompt('Введите имя')
+    let age = +prompt('Введите возраст')
 
-    
-const users = {
-    'Ползователь 1': {
-        name: name1,
-        age: age1,    
-    },
-    'Ползователь 2': {
-        name: name2,
-        age: age2,    
-    },
-    'Ползователь 3': {
-        name: name3,
-        age: age3,    
-    },
-    'Ползователь 4': {
-        name: name4,
-        age: age4,
-    },
-    'Ползователь 5': {
-        name: name5,
-        age: age5,
+    obj[i] = {
+        name: name,
+        age: age
     }
 }
-    
-    console.log('Пользователь 1 ' + 'Имя: ' + name1 + ' Возраст: ' + age1)
-    console.log('Пользователь 2 ' + 'Имя: ' + name2 + ' Возраст: ' + age2)
-    console.log('Пользователь 3 ' + 'Имя: ' + name3 + ' Возраст: ' + age3)
-    console.log('Пользователь 4 ' + 'Имя: ' + name4 + ' Возраст: ' + age4)
-    console.log('Пользователь 5 ' + 'Имя: ' + name5 + ' Возраст: ' + age5)
+
+for(const key in obj) {
+    console.log(`Пользователь ${key}`);
+    for(const newKey in obj[key]) {
+        console.log(newKey == 'name' ? `Ваше имя ${obj[key][newKey]}` : `Ваш возраст ${obj[key][newKey]}`);
+    }
+}
